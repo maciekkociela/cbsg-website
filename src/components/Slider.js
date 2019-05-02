0e9141d5e64792abba443ec716a3016266ed2fc3
maciekkociela <kocielam@gmail.com>
2019-05-02 16:46:48 +0200

    Slider sass setup

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import { Fade } from 'react-slideshow-image';
+import StringToHtml from '../components/StringToHtml'
+import BackgroundImage from 'gatsby-background-image'
 
-function createMarkup(html) {
-  return { __html: html }
-}
+import styles from './Slider.module.scss'
+  
+  const fadeProperties = {
+    duration: 2000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: false
+  }
 
-function Slider(props) {
-  const { html } = props
-  return <span className={html} dangerouslySetInnerHTML={createMarkup(html)} />
-}
+const Slider = (props) => {
+    return (
+      <Fade {...fadeProperties}>
+        {props.slides.map( (slide) =>
+                <div className={styles.eachFade}>
+                  
+                  
+                    <BackgroundImage Tag="section"
+                           className="className"
+                           fluid={slide.node.featured_media.localFile.childImageSharp.sizes}
+                           backgroundColor={`#040e18`}
+                    >   
+                        <div className={styles.slide}>
+                            <h1><StringToHtml html={slide.node.title} /></h1>
+                        </div>
+                    </BackgroundImage>
+                  
+                </div>
+        )}
+      </Fade>
+      
+    )
+  }
 
 export default Slider
